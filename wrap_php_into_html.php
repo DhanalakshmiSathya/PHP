58cda9a59e8da5d2130d7dd0a35607d695a5c849
DhanalakshmiSathya <dhanalakshmign2003@outlook.com>
2024-10-18 12:50:00 +0530

    Embedding javascript into  php

---
+++ wrap_php_into_html.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p <?php echo 'style="color:pink"' ?>>red</p>
+<?php 
+    echo"hello";
+    ?>
+    <!-- javascript -->
+     <script>
+        alert("hi php learners!")
+     </script>
+
+     <!-- wrapping php inside javascript language -->
+      <script>
+        <?php echo" alert('hello it\'s 12pm')"; ?>
+      </script>
+</body>
+</html>
